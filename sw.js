092de273eb8c61c5b927fd40effe1b4a4d3cab73
sankusodhi <sankuramsodhi23@navgurukul.org>
2025-02-14 19:19:25 +0530

    update123

--- sw.js
+++ sw.js
@@ -1,28 +1,22 @@
-const CACHE_NAME = "pwa-cache-v1";
-const urlsToCache = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/script.js",
-    "/manifest.json",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
-];
-
-// Install Service Worker
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
+        caches.open('pwa-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/script.js',
+                '/icons/icon-192x192.png',
+                '/icons/icon-512x512.png',
+            ]);
         })
     );
 });
 
-// Fetch Data from Cache or Network
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
         })
     );
 });
